2dc52d85837b0b5fd6352258d94e1efe88aa1e4c
Tung Nguyen <tungnguyen@success-ss.com.vn>
2015-07-28 15:13:54 +0700

    import code

---
+++ sites/tungnguyen/themes/tungnguyen/template.php
@@ -0,0 +1,31 @@
+<?php
+
+function tungnguyen_preprocess_node(&$vars) {
+	$vars['submitted'] =  t('Posted by !username on !datetime',
+		array(
+			'!username' => $vars['name'],
+			'!datetime' => $vars['date'],
+		)
+	);
+	
+	$vars['posted'] = t('By !username',
+		array(
+			'!username' => $vars['name']
+		)		
+	);
+	
+	if (isset($vars['content']['links']['node']['#links']['node-readmore'])) {
+	
+		// make a copy of the old link and change the link's title
+		$readmore = $vars['content']['links']['node']['#links']['node-readmore'];
+		$readmore['title'] = t('Continue Reading &raquo;');
+	
+		// remove the old link
+		unset($vars['content']['links']['node']['#links']['node-readmore']);
+	
+		// creat a new link under a different name
+		$vars['content']['links']['node']['#links']['node-readmore'] = $readmore;
+		
+		unset($vars['content']['links']['comment']['#links']['comment-add']);
+	}
+}
---
+++ sites/tungnguyen/themes/tungnguyen/theme-settings.php
@@ -0,0 +1,59 @@
+<?php
+function tungnguyen_form_system_theme_settings_alter(&$form, &$form_state) 
+{
+	$form['social'] = array (
+		'#type'		=> 'fieldset',
+		'#title'		=> 'Social setting',
+		'#collapsible'	=> true,
+		'#collapsed'	=> false,
+	);
+	$form['social']['social_display'] = array(
+		'#type'		=> 'checkbox',
+		'#title'		=> 'Dispaly social icon',
+		'#default_value'	=> theme_get_setting('social_display', 'tungnguyen'),
+		'#description'	=> 'Checked this option to show social icon.',
+	);
+	$form['social']['facebook_url'] = array(
+		'#type'		=> 'textfield',
+		'#title'		=> 'Facebook Url',
+		'#default_value'	=> theme_get_setting('facebook_url', 'tungnguyen'),
+		'#description'	=> 'Enter facebook url',
+	);
+	$form['social']['twitter_url'] = array(
+			'#type'		=> 'textfield',
+			'#title'		=> 'Twitter Url',
+			'#default_value'	=> theme_get_setting('twitter_url', 'tungnguyen'),
+			'#description'	=> 'Enter Twitter url',
+	);
+	$form['social']['googleplus_url'] = array(
+			'#type'		=> 'textfield',
+			'#title'		=> 'googleplus Url',
+			'#default_value'	=> theme_get_setting('googleplus_url', 'tungnguyen'),
+			'#description'	=> 'Enter google plus url',
+	);
+	$form['social']['cm_url'] = array(
+			'#type'		=> 'textfield',
+			'#title'		=> 'CM Url',
+			'#default_value'	=> theme_get_setting('cm_url', 'tungnguyen'),
+			'#description'	=> 'Enter CM url',
+	);
+	
+	$form['book_room_url'] = array(
+			'#type'		=> 'textfield',
+			'#title'		=> 'Book room url',
+			'#default_value'	=> theme_get_setting('book_room_url', 'tungnguyen'),
+			'#description'	=> 'This is url for user book room.',
+	);
+	$form['red_dragon_url'] = array(
+			'#type'		=> 'textfield',
+			'#title'		=> 'Red Dragon Hotel url',
+			'#default_value'	=> theme_get_setting('red_dragon_url', 'tungnguyen'),
+			'#description'	=> 'This is url for Red Dragon hotel.',
+	);
+	$form['google_api_key'] = array(
+			'#type'		=> 'textfield',
+			'#title'		=> 'Google API Key',
+			'#default_value'	=> theme_get_setting('google_api_key', 'tungnguyen'),
+			'#description'	=> 'Google API Key.',
+	);
+}
